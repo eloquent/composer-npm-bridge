ecca31affefbe8f1228df5063c9757e55c41f266
Erin Millard <ezzatron@gmail.com>
2012-10-09 13:06:49 +1000

    Implemented prototype.

--- src/Eloquent/Composer/NPMBridge/NPMBridge.php
+++ src/Eloquent/Composer/NPMBridge/NPMBridge.php
@@ -11,8 +11,14 @@
 
 namespace Eloquent\Composer\NPMBridge;
 
+use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use Icecave\Isolator\Isolator;
+use RuntimeException;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
 
 class NPMBridge
 {
@@ -50,10 +56,39 @@ class NPMBridge
     }
 
     /**
+     * @param ExecutableFinder|null $executableFinder
+     * @param Isolator|null $isolator
+     */
+    public function __construct(
+        ExecutableFinder $executableFinder = null,
+        Isolator $isolator = null
+    ) {
+        if (null === $executableFinder) {
+            $executableFinder = new ExecutableFinder;
+        }
+
+        $this->executableFinder = $executableFinder;
+        $this->isolator = Isolator::get($isolator);
+    }
+
+    /**
+     * @return ExecutableFinder
+     */
+    public function executableFinder()
+    {
+        return $this->executableFinder;
+    }
+
+    /**
      * @param Event $event
      */
     public function postInstall(Event $event)
     {
+        $io = $event->getIO();
+        $io->write('Installing NPM dependencies...', true);
+
+        $this->executeNpm(array('install'), $io);
+        $this->executeNpm(array('shrinkwrap'), $io);
     }
 
     /**
@@ -61,5 +96,84 @@ class NPMBridge
      */
     public function postUpdate(Event $event)
     {
+        $io = $event->getIO();
+        $io->write('Updating NPM dependencies...', true);
+
+        $this->unwrap($io);
+        $this->executeNpm(array('update'), $io);
+        $this->executeNpm(array('shrinkwrap'), $io);
     }
+
+    /**
+     * @param array<string> $arguments
+     * @param IOInterface $io
+     */
+    protected function executeNpm(array $arguments, IOInterface $io)
+    {
+        $npmProcess = $this->createNpmProcess($arguments);
+        $isolator = $this->isolator;
+        $npmProcess->run(function ($type, $buffer) use ($isolator, $io) {
+            if (Process::ERR === $type) {
+                $isolator->fwrite(STDERR, $buffer);
+            } else {
+                $io->write($buffer, false);
+            }
+        });
+    }
+
+    /**
+     * @param IOInterface $io
+     */
+    protected function unwrap(IOInterface $io)
+    {
+        $io->write('Removing NPM shrinkwrap... ', false);
+        if ($this->isolator->is_file('./npm-shrinkwrap.json')) {
+            $this->isolator->unlink('./npm-shrinkwrap.json');
+            $io->write('done.', true);
+        } else {
+            $io->write('nothing to do.', true);
+        }
+    }
+
+    /**
+     * @param array<string> $arguments
+     *
+     * @return Process
+     */
+    protected function createNpmProcess(array $arguments)
+    {
+        array_unshift($arguments, $this->npmPath());
+        $processBuilder = $this->createProcessBuilder($arguments);
+
+        return $processBuilder->getProcess();
+    }
+
+    /**
+     * @return string
+     */
+    protected function npmPath()
+    {
+        if (null === $this->npmPath) {
+            $this->npmPath = $this->executableFinder()->find('npm');
+            if (null === $this->npmPath) {
+                throw new RuntimeException('Unable to locate npm executable.');
+            }
+        }
+
+        return $this->npmPath;
+    }
+
+    /**
+     * @param array<string> $arguments
+     *
+     * @return ProcessBuilder
+     */
+    protected function createProcessBuilder(array $arguments)
+    {
+        return new ProcessBuilder($arguments);
+    }
+
+    private $executableFinder;
+    private $isolator;
+    private $npmPath;
 }
